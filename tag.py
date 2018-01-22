ef86d61a73934893406913044ec8935267bd726f
Jenna Kanerva <jmnybl@utu.fi>
2018-01-22 18:22:57 +0200

    tagging should work

--- tag.py
+++ tag.py
@@ -7,6 +7,7 @@ import torch.nn.functional as F
 import torch.optim as optim
 from numpy.random import shuffle
 import pickle
+from itertools import zip_longest
 
 from data import POSdata, TorchData
 from models import SequenceTagger
@@ -16,6 +17,9 @@ from text_classifier_torch.t2i import T2I as text_vectorizer
 from text_classifier_torch.t2i import to_torch_long_tensor,torch_minibatched_2dim
 
 
+WARNING='\033[91m'
+END_WARNING='\033[0m'
+
 def predictions2text(predictions,label_vectorizer):
     text_labels=[]
     scores,predictions_=predictions.max(2)
@@ -24,12 +28,10 @@ def predictions2text(predictions,label_vectorizer):
         text_labels.append(max_labels)
     return text_labels
 
-
+ID,FORM,LEMMA,UPOS,POS,FEAT,HEAD,DEPREL,DEPS,MISC=range(10)
 #def predict(data,model,label_vectorizer,targets,args,sentences,verbose=False):
-def predict():
-    correct=0
-    total=0
-    _word_in,_char_in=data
+def predict(input_data, model, label_vectorizer, conllu_sentences):
+    _word_in,_char_in=input_data
     number_of_batches=_word_in.size(1)
     for batch_id in range(number_of_batches):
         word_batch=autograd.Variable(_word_in[:,batch_id,:])
@@ -39,36 +41,39 @@ def predict():
             char_batch=char_batch.cuda()
         model.eval()
         predictions=predictions2text(model(word_batch,char_batch),label_vectorizer)
-        target_batch=targets[batch_id*args.batch_size:batch_id*args.batch_size+args.batch_size]
-        sentence_batch=sentences[batch_id*args.batch_size:batch_id*args.batch_size+args.batch_size]
-        for psent,sent,sentence in zip(predictions,target_batch,sentence_batch):
-            if verbose:
-                print("PRED:",psent)
-                print("GOLD:",sent)
-                print("INPUT:",sentence,"\n")
-            for ptoken,token in zip(psent,sent):
-                if token=="<START>" or token=="<END>":
-                    continue
-                if token=="__PADDING__":
-                    break
-                if token==ptoken:
-                    correct+=1
-                total+=1
-    return correct/total*100 if total!= 0 else 0
+        conllu_batch=conllu_sentences[batch_id*args.batch_size:batch_id*args.batch_size+args.batch_size]
+        for psent,(comm,conllu) in zip_longest(predictions,conllu_batch,fillvalue=[]): # this is one sequence, i.e. sentence
+            if not conllu:
+                #print
+                break
+            sentence=[]
+            for i, token in enumerate(conllu):
+                ptoken=psent[i+1] # remove start marker to keep aligned
+                if ptoken=="<START>" or ptoken=="<END>" or ptoken=="__PADDING__":
+                    ptoken="_"
+                token[UPOS]=ptoken
+                sentence.append(token)
+            for c in comm:
+                print(c)
+            for token in sentence:
+                print("\t".join(t for t in token))
+            print("")
 
 
 
 
-def train(args):
+
+def predict_eval(args):
 
     # data
     posdata=POSdata()
     input_sentences,input_labels=posdata.read_data(args.input_file, 100000000, count_words=False)
+    conllu_sentences=[(c,s) for c, s in posdata.conllu_reader(open(args.input_file,"rt"))]
 
     with open(os.path.join(args.model_dir,"torchdata_vectorizers.pkl"),"rb") as f:
         torchdata=pickle.load(f) # class to turn text into torch style minibatches
     
-    # TODO: max sequence lengths?
+    # TODO: max sequence lengths? Should collect from the data?
     data_batches_word, data_batches_char, data_batches_label=torchdata.prepare_torch_data(input_sentences, input_labels, args.batch_size, args.max_seq_len, args.max_seq_len_char, train=False, shuffle=False) # sentences, labels, batch_size, seq_words, seq_chars, train=True, shuffle=False
 
 
@@ -83,8 +88,7 @@ def train(args):
         print("ACCURACY:",acc)
         
     else:
-        pass
-        #outputs=predict()
+        predict((data_batches_word, data_batches_char), model, torchdata.label_vectorizer, conllu_sentences)
 
 
 
@@ -108,4 +112,8 @@ if __name__=="__main__":
     
     args = parser.parse_args()
 
-    accuracy=train(args)
+    if args.batch_size!=1:
+        print(WARNING, "Batching does not work properly right now, forcing batch_size to one temporarily.", END_WARNING, file=sys.stderr)
+        args.batch_size=1
+
+    predict_eval(args)
